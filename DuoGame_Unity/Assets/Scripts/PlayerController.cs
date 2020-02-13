1ae07cf9a45863ae671b03bbb4c6b80e0f12bd14
Thijmen <Thijmenh01@gmail.com>
2020-02-14 00:01:14 +0100

    working

--- DuoGame_Unity/Assets/Scripts/PlayerController.cs
+++ DuoGame_Unity/Assets/Scripts/PlayerController.cs
@@ -3,64 +3,104 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent( typeof( Rigidbody ) )]
+[RequireComponent( typeof( Rigidbody2D ) , typeof( Collider2D ) )]
 public class PlayerController : MonoBehaviour {
 
-    [SerializeField] private float moveSpeed = 7.5f;
-    [SerializeField] private float smoothMoveTime = 0.1f;
-    [SerializeField] private float turnSpeed = 8;
-    RaycastHit2D ray;
-    Rigidbody rb;
+    //#region old
+    //[SerializeField] private float moveSpeed = 7.5f;
+    //[SerializeField] private float smoothMoveTime = 0.1f;
+    //[SerializeField] private float turnSpeed = 8;
+    //RaycastHit2D ray;
+    //private Vector3 moveInput;
+    //private float angle;
+    //private float smoothInputMagnitude;
+    //private float smoothMoveVelocity;
+    //#endregion
+
+    private Rigidbody2D m_rigidbody2D;
+    private Collider2D m_collider2D;
     private Vector2 moveInput;
-    private float angle;
-    private float smoothInputMagnitude;
-    private float smoothMoveVelocity;
+    private float velocity = 1000;
 
-    void Start() {
-        rb = GetComponent<Rigidbody>();
-    }
-
-    void Update() {
-
-        ray = Physics2D.Raycast( transform.position , Vector2.down );
-
-        if(ray.collider.tag == "Track") {
-            Debug.DrawRay( transform.position , transform.TransformDirection( Vector3.down ) * ray.distance , Color.green );
-            print( "hit" + ray.collider.name );
-        }
-
-        if(ray.collider.tag == "Boost") {
-            Debug.DrawRay( transform.position , transform.TransformDirection( Vector3.down ) * ray.distance , Color.blue );
-            print( "hit" + ray.collider.name );
-        }
-
-        if(ray.collider.tag == "Slow") {
-            Debug.DrawRay( transform.position , transform.TransformDirection( Vector3.down ) * ray.distance , Color.red );
-            print( "hit" + ray.collider.name );
-        }
-    }
-
-    #region Movement
-    private void FixedUpdate() {
-        rb.MoveRotation( Quaternion.Euler( Vector3.up * angle ) );
-        Move();
+    private void Start() {
+        m_rigidbody2D = GetComponent<Rigidbody2D>();
+        m_collider2D = GetComponent<Collider2D>();
     }
 
     public void OnMovement(InputAction.CallbackContext context) {
         moveInput = context.ReadValue<Vector2>();
     }
 
-    private void Move() {
-        Vector3 _inputDirection = new Vector3( moveInput.x , 0 , moveInput.y );
-
-        float _inputMagnitude = _inputDirection.magnitude;
-        smoothInputMagnitude = Mathf.SmoothDamp( smoothInputMagnitude , _inputMagnitude , ref smoothMoveVelocity , smoothMoveTime );
-
-        float _targetAngle = Mathf.Atan2( _inputDirection.x , _inputDirection.z ) * Mathf.Rad2Deg;
-        angle = Mathf.LerpAngle( angle , _targetAngle , Time.deltaTime * turnSpeed * _inputMagnitude );
-
-        Vector3 velocity = transform.forward * moveSpeed * smoothInputMagnitude;
-        rb.MovePosition( rb.position + velocity * Time.deltaTime );
+    private void FixedUpdate() {
+        m_rigidbody2D.velocity = transform.up * velocity * Time.deltaTime * moveInput.y;
+        m_rigidbody2D.AddTorque( moveInput.x * m_rigidbody2D.velocity.y );
     }
+
+    #region old
+
+    //void Start() {
+    //    rb = GetComponent<Rigidbody2D>();
+    //}
+
+    //void Update() {
+
+    //    ray = Physics2D.Raycast( transform.position , Vector2.down );
+
+    //    if(ray.collider.tag == "Track") {
+    //        Debug.DrawRay( transform.position , transform.TransformDirection( Vector3.down ) * ray.distance , Color.green );
+    //        print( "hit" + ray.collider.name );
+    //    }
+
+    //    if(ray.collider.tag == "Boost") {
+    //        Debug.DrawRay( transform.position , transform.TransformDirection( Vector3.down ) * ray.distance , Color.blue );
+    //        print( "hit" + ray.collider.name );
+    //    }
+
+    //    if(ray.collider.tag == "Slow") {
+    //        Debug.DrawRay( transform.position , transform.TransformDirection( Vector3.down ) * ray.distance , Color.red );
+    //        print( "hit" + ray.collider.name );
+    //    } else {
+    //        print( "hit nothing" );
+    //    }
+    //}
+
+    //#region Movement
+    //private void FixedUpdate() {
+    //    Move();
+    //}
+
+    //public void OnMovement(InputAction.CallbackContext context) {
+    //    moveInput = context.ReadValue<Vector2>();
+    //}
+
+    //private void Move() {
+
+    //    float _inputMagnitude = moveInput.magnitude;
+    //    smoothInputMagnitude = Mathf.SmoothDamp( smoothInputMagnitude , _inputMagnitude , ref smoothMoveVelocity , smoothMoveTime );
+
+    //    float _targetAngle = Mathf.Atan2( moveInput.x , moveInput.z ) * Mathf.Rad2Deg;
+    //    angle = Mathf.LerpAngle( angle , _targetAngle , Time.deltaTime * turnSpeed * _inputMagnitude );
+
+    //    rb.MoveRotation( Quaternion.Euler( Vector3.forward * angle ) );
+
+    //    Vector3 velocity = transform.right * moveSpeed * smoothInputMagnitude;
+
+    //    rb.MovePosition( (Vector3)rb.position + velocity * Time.deltaTime );
+
+
+
+    //    //Vector3 _inputDirection = new Vector3( moveInput.x , 0 , moveInput.z );
+
+    //    //float _inputMagnitude = _inputDirection.magnitude;
+    //    //smoothInputMagnitude = Mathf.SmoothDamp( smoothInputMagnitude , _inputMagnitude , ref smoothMoveVelocity , smoothMoveTime );
+
+    //    //float _targetAngle = Mathf.Atan2( _inputDirection.x , _inputDirection.y ) * Mathf.Rad2Deg;
+    //    //angle = Mathf.LerpAngle( angle , _targetAngle , Time.deltaTime * turnSpeed * _inputMagnitude );
+
+    //    //Vector3 velocity = transform.up * moveSpeed * smoothInputMagnitude;
+    //    //rb.MovePosition((Vector3) rb.position + velocity * Time.deltaTime );
+    //}
+    //#endregion
+
     #endregion
 }
---
+++ DuoGame_Unity/Assets/Scripts/ServerRequestTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerRequestTest : MonoBehaviour {
+    private int counter;
+
+    void Update() {
+        if(Input.GetKeyDown( KeyCode.Space )) {
+            StartCoroutine( GetFruitAsync() );
+            counter++;
+        }
+    }
+
+    private IEnumerator GetFruitAsync() {
+        WWWForm form = new WWWForm();
+        form.AddField( "counter" , counter );
+        UnityWebRequest www = UnityWebRequest.Post( "http://127.0.0.1/edsa-Database/example.php" , form );
+        yield return www.SendWebRequest();
+        Debug.Log( www.downloadHandler.text );
+    }
+}
